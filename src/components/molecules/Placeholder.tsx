383f7ac397d88f5d548bf576447ec6a39d109635
Jason Son <jasonson2004@gmail.com>
2025-08-09 17:12:51 -0700

    add: placeholder and some empty palceholder files [connector, frame]

---
+++ src/components/molecules/Placeholder.tsx
@@ -0,0 +1,24 @@
+// components/Placeholder
+// Is rendered if no available frames are present.
+
+import Image from "next/image";
+
+const Placeholder = () => {
+  return <div className="flex flex-col gap-2">
+    <div className="flex flex-row">
+      <Image src={""} alt={""} />
+      <h1 className="text-xl">
+        <span className="text-[#1E1919]">Hold down anywhere</span>
+        <span className="text-[#9D9D9D]">{" "}to create a new frame, or get started with</span>
+      </h1>
+    </div>
+    <div className="flex flex-row">
+      <button className="">Create a tool</button>
+      <button>Build a website</button>
+      <button>Start from a template</button>
+      <button>View other&apos;s work</button>
+    </div>
+  </div>
+};
+
+export { Placeholder };
